f8fd17d97bd49ec2d6a19bb0f6d4dbc60c0590c5
kuper0201 <kuper171@gmail.com>
2025-02-10 20:49:49 +0900

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -Syu --noconfirm linux-lts-headers lightdm lightdm-gtk-greeter enlightenment nvidia-dkms docker networkmanager nano
+pacman -Syu --noconfirm linux-lts-headers lightdm lightdm-gtk-greeter openbox lxqt-panel nvidia-dkms docker networkmanager nano
 
 # check install_yay.sh
 systemctl enable NetworkManager
