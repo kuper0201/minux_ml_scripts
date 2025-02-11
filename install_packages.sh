ca8e83f24b312313a49a67d6261039ebc51770d8
kuper0201 <kuper171@gmail.com>
2025-02-11 14:19:21 +0900

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pacman -Syu --noconfirm linux-lts-headers lightdm lightdm-gtk-greeter openbox lxqt-panel nvidia-dkms docker networkmanager nano
+pacman -Syu --needed --noconfirm linux-lts-headers lightdm lightdm-gtk-greeter nvidia-dkms docker networkmanager git base-devel nano
+pacman -Syu --needed --noconfirm xterm network-manager-applet openbox lxqt-panel pcmanfm-gtk3 lxappearance alacritty kvantum kvantum-theme-materia materia-gtk-theme kvantum-qt5 qt5ct qt6ct
 
 # check install_yay.sh
 systemctl enable NetworkManager
